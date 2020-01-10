1e211f3a95b02dfef3d749f152106d59c928f394
chefboyrdeuce <jpldeuce@gmail.com>
2020-01-09 22:36:45 -0500

    add redux dependencies, created empty reducers and initial state

--- src/index.js
+++ src/index.js
@@ -5,5 +5,25 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import { appReducer } from './reducers/index';
+
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+        <App />, 
+    rootElement)
+;
+
+
+// let store = createStore(appReducer, applyMiddleware(logger, thunk));
+
+// const rootElement = document.getElementById('root');
+// ReactDOM.render(
+//     <Provider store={store}>
+//         <App />
+//     </Provider>, 
+//     rootElement)
+// ;
